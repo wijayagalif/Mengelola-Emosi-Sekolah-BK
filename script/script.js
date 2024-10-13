c0ba4a608b821bec75d4b83f4de5a37e776be614
Wijanay <wijayagalif@gmail.com>
2024-10-13 20:03:36 +0700

    penambahan mobile toggle

--- script/script.js
+++ script/script.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('themeToggle');
+    const toggleButton = document.querySelector('.toggle-button');
+    const navbar = document.querySelector('nav ul')
+
+    // Event listener untuk tema
     themeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
         themeToggle.classList.toggle('active');
@@ -9,5 +13,10 @@ document.addEventListener('DOMContentLoaded', () => {
 
         sunIcon.style.display = sunIcon.style.display === 'none' ? 'block' : 'none';
         moonIcon.style.display = moonIcon.style.display === 'none' ? 'block' : 'none';
+    });
+
+    // Event listener untuk toggle navbar
+    toggleButton.addEventListener('click', () => {
+        navbar.classList.toggle('active');
     });
 });
