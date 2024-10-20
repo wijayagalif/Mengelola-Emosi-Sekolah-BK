404fc5f50d4ecddef875a0d758352c65fe841808
Wijanay <wijayagalif@gmail.com>
2024-10-20 21:02:39 +0700

    perbaikan css

--- script/script.js
+++ script/script.js
@@ -2,14 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('themeToggle');
     const toggleButton = document.querySelector('.toggle-button');
     const navbar = document.querySelector('nav ul')
+    const sunIcon = themeToggle.querySelector('.fa-sun');
+    const moonIcon = themeToggle.querySelector('.fa-moon');
 
     // Event listener untuk tema
     themeToggle.addEventListener('click', () => {
         document.body.classList.toggle('dark-mode');
         themeToggle.classList.toggle('active');
-
-        const sunIcon = themeToggle.querySelector('.fa-sun');
-        const moonIcon = themeToggle.querySelector('.fa-moon');
 
         sunIcon.style.display = sunIcon.style.display === 'none' ? 'block' : 'none';
         moonIcon.style.display = moonIcon.style.display === 'none' ? 'block' : 'none';
